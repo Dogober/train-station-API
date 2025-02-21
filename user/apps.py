672f0a3d423b15e0590394455d118a5c28b87f82
dogober <dogoberrr@gmail.com>
2025-02-21 15:47:14 +0200

    changed crew app to user and added user, station app to installed apps

--- user/apps.py
+++ user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CrewConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crew'
+    name = 'user'
