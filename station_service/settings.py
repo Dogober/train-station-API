850d0fa604e645e86ab879f707d71e3ea1497caf
dogober <dogoberrr@gmail.com>
2025-02-23 15:47:02 +0200

    add pagination to rest_framwork into the settings

--- station_service/settings.py
+++ station_service/settings.py
@@ -133,12 +133,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    # "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "30/day"},
     "DEFAULT_PERMISSION_CLASSES": [
         "station.permissions.IsAdminOrIfAuthenticatedReadOnly"
     ],
